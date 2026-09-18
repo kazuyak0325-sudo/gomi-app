4d90a4a298c637c41defcab343a9e5b83b3ca500
kazuyak0325 <kazuya.k0325@gmail.com>
2026-09-18 14:07:01 +0900

    Make offline caching resilient to partial failures (v2 cache)

--- kuro/sw.js
+++ kuro/sw.js
@@ -1,17 +1,21 @@
-﻿const CACHE_NAME = "gomi-app-kuro-v1";
+﻿const CACHE_NAME = "gomi-app-kuro-v2";
 const ASSETS = ["./", "index.html", "manifest.json", "icon.png", "../style.css", "../app.js"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-      .catch(() => {})
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(ASSETS.map((url) => cache.add(url).catch(() => {})));
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
