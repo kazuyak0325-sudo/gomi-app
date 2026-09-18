39f043cb888418713978e1a00ba61a80b45168ab
kazuyak0325 <kazuya.k0325@gmail.com>
2026-09-18 10:31:59 +0900

    gomi-app initial PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "gomi-app-v1";
+const ASSETS = ["./", "index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
