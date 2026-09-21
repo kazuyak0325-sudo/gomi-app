7ebc5cbe2c6fb8cc1649f61356ebcbf898745828
kazuyak0325 <kazuya.k0325@gmail.com>
2026-09-21 09:56:55 +0900

    Publish 紫コース（火・金） (108 stations) via admin tool v0.10

---
+++ murasaki/kakin/sw.js
@@ -0,0 +1,32 @@
+﻿const CACHE_NAME = "gomi-app-murasaki-kakin-1789952215";
+const ASSETS = ["./", "index.html", "manifest.json", "icon.png", "../../style.css?v=1789952215", "../../app.js?v=1789952215"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(ASSETS.map((url) => cache.add(url).catch(() => {})));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
